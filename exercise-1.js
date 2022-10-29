d102021017876a3de966e1eecba8bab34e7aa8c9
Chiara Arrivas <ritarrivas96@gmail.com>
2022-10-29 12:24:17 +0200

    Objects Methods

---
+++ exercise-1.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+let pair= Object.entries(person);
+
+for(let entry of pair) {
+  let[key, val] = entry;
+  console.log(key + ": " + val)
+} 
+// Print values of person using Object.keys
